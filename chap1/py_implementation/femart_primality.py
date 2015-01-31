7be36375197380c9d4c10266aeecd815b47afe6a
SamuraiT <t.yasukawa01@gmail.com>
2015-01-31 16:32:29 +0900

    add femart test implementation of primality(slow version)

---
+++ chap1/py_implementation/femart_primality.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+# coding: utf-8
+"""
+Copyright 2014 Tatsuro Yasukawa.
+
+Distributed under the GNU General Public License at
+gnu.org/licenses/gpl.html.
+"""
+
+
+def main():
+    pass
+
+#! /usr/bin/env python
+# coding: utf-8
+"""
+Copyright 2014 Tatsuro Yasukawa.
+
+Distributed under the GNU General Public License at
+gnu.org/licenses/gpl.html.
+"""
+import sys
+sys.setrecursionlimit(10000)
+from random import randint
+
+def main():
+    pass
+
+def is_prime(n, times=10):
+    """
+    >>> is_prime(10)
+    False
+    >>> is_prime(17)
+    True
+
+    #is_prime(27644437)
+    #True
+    """
+    for i in range(times):
+        if fermat_test(n) is False:
+            return False
+    return True
+
+def fermat_test(n):
+    a = randint(2, n-1) #including both end points
+    return expmod(a, n, n) == a
+
+def expmod(base, exp, n):
+    """
+    calc expotional modulo n:
+    `base^exp modulo n`
+    By only doing so, this is too slow
+    """
+    return (base**exp) % n
+
+if __name__ == '__main__':
+    pass
+
